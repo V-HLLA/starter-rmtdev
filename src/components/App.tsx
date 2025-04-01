aec713b8c1d2b6fa245c6c0bc6e33c02d0e8d178
V <downfiresword@gmail.com>
2025-03-31 23:12:46 -0300

    Created Client-side Pagination.

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -16,13 +16,31 @@ import { useDebounce, useJobSearchResults } from "../lib/hooks";
 import { Toaster } from "react-hot-toast";
 
 function App() {
+  //state
   const [searchText, setSearchText] = useState("");
   const debouncedValue = useDebounce(searchText, 300);
   const { jobItems, isLoading } = useJobSearchResults(debouncedValue);
+  const [currentPage, setCurrentPage] = useState(1);
 
-  const jobItemsSliced = jobItems?.slice(0, 7) || [];
+  // derived / computed state
+  const jobItemsSliced =
+    jobItems?.slice(currentPage * 7 - 7, currentPage * 7) || [];
   const displayedCount = jobItemsSliced?.length || 0;
   const resultsCount = jobItems?.length || 0;
+  const totalNumberOfPages = resultsCount / 7;
+
+  // event handlers / actions
+  const handleChangePage = (direction: "next" | "previous") => {
+    if (direction === "next") {
+      setCurrentPage((prev) => prev + 1);
+    } else if (direction === "previous") {
+      if (currentPage === 1) {
+        return;
+      } else {
+        setCurrentPage((prev) => prev - 1);
+      }
+    }
+  };
 
   return (
     <>
@@ -48,7 +66,11 @@ function App() {
           </SidebarTop>
 
           <JobList jobItems={jobItemsSliced} isLoading={isLoading} />
-          <PaginationControls />
+          <PaginationControls
+            onClick={handleChangePage}
+            Page={currentPage}
+            totalNumberOfPages={totalNumberOfPages}
+          />
         </Sidebar>
 
         <JobItemContent />
--- src/components/PaginationControls.tsx
+++ src/components/PaginationControls.tsx
@@ -1,3 +1,40 @@
-export default function PaginationControls() {
-  return <section className="pagination"></section>;
+import { ArrowLeftIcon, ArrowRightIcon } from "@radix-ui/react-icons";
+
+type TPaginationCrontrolsProps = {
+  onClick: (direction: "next" | "previous") => void;
+  Page: number;
+  totalNumberOfPages: number;
+};
+
+export default function PaginationControls({
+  onClick,
+  Page,
+  totalNumberOfPages,
+}: TPaginationCrontrolsProps) {
+  return (
+    <section className="pagination">
+      {Page >= 2 ? (
+        <button
+          onClick={(e) => {
+            onClick("previous"), e.currentTarget.blur();
+          }}
+          className="pagination__button"
+        >
+          <ArrowLeftIcon />
+          Page {Page - 1}
+        </button>
+      ) : null}
+      {Page < totalNumberOfPages && (
+        <button
+          onClick={(e) => {
+            onClick("next"), e.currentTarget.blur();
+          }}
+          className="pagination__button pagination__button--next"
+        >
+          <ArrowRightIcon />
+          Page {Page + 1}
+        </button>
+      )}
+    </section>
+  );
 }
